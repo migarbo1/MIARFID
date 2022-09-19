e158287817ff3c5a293987e0fd30a0378d8e3f28
Miguel <miguel.garciabohigues@hotmail.com>
2022-09-19 17:25:45 +0200

    pract 1 GPC

--- GPC/P1/webgl/js/drawPoints.js
+++ GPC/P1/webgl/js/drawPoints.js
@@ -1,41 +1,119 @@
-/* 
-    seminar number one, draw points with VBOs
+/*
+    Seminario #1: Dibujar puntos con VBOs
 */
 
-//vertex shader
-const V_SHADER_SOURCE = `
-    attribute vec3 pointPosition;
+// Shader de vertices
+const VSHADER_SOURCE = `
+    attribute vec3 posicion;
+    attribute vec3 color;
+    varying vec3 vColor;
     void main(){
-        gl_Position = vec4(pointPosition,1.0);
-        gl_pointSize = 10.0;
+        gl_Position = vec4(posicion,1.0);
+        gl_PointSize = 10.0;
+        vColor = color;
     }
 `
-//fragment shader
-const F_SHADER_SOURCE = `
-    unform highp vec3 color;
+
+// Shader de fragmentos
+const FSHADER_SOURCE = `
+    varying highp vec3 vColor;
     void main(){
-        gl_FragColor = vec4(color, 1.0); //RGBA
+        gl_FragColor = vec4(vColor,1.0);
     }
 `
-const cliscs = [];
-let fragmentColor;
+// Globales
+const clicks = [];
 
-function main(){
-    //retrieve canvas
-    const canvas = document.getElementById('canvas');
-    const gl = getWebGLContext(canvas)
+const colors = []; 
 
-    //load shaders into the program
-    if(!initShaders(gl, V_SHADER_SOURCE, F_SHADER_SOURCE)){
-        console.error("error loading shaders");
+let colorFragmento;
+
+function main()
+{
+    // Recupera el lienzo
+    const canvas = document.getElementById("canvas");
+    const gl = getWebGLContext( canvas );
+
+    // Cargo shaders en programa de GPU
+    if(!initShaders(gl,VSHADER_SOURCE,FSHADER_SOURCE)){
+        console.log("there's an error with the shaders");
     }
 
-    //canvas eraser color
-    gl.clearColor(0.0,0.0,0.3,1.0)
+    // Color de borrado del lienzo
+    gl.clearColor(0.0, 0.0, 0.3, 1.0);
 
-    //locate position parameter in shader
-    const coord = gl.getAttribLocation(gl.program, pointPosition);
+    // Registrar la call-back del click del raton
+    canvas.onmousedown = function(evento){ click(evento,gl,canvas); };
 
-    //create buffer and stuff
-    const bufferVertex = gl.createBuffer();
+    // Dibujar
+    render( gl );
+    
+}
+
+function click( evento, gl, canvas )
+{
+    // Recuperar la posicion del click
+    // El click devuelve la x,y en el sistema de referencia
+    // del documento. Los puntos que se pasan al shader deben
+    // de estar en el cuadrado de lado dos centrado en el canvas
+
+    let x = evento.clientX;
+    let y = evento.clientY;
+    const rect = evento.target.getBoundingClientRect();
+
+    // Conversion de coordenadas al sistema webgl por defecto
+    x = ((x-rect.left)-canvas.width/2) * 2/canvas.width;
+    y = ( canvas.height/2 - (y-rect.top)) * 2/canvas.height;
+
+	
+	// Guardar las coordenadas y copia el array
+	clicks.push(x); clicks.push(y); clicks.push(0.0);
+    colors.push(1-getColor(x,y)); //R
+    colors.push(1-getColor(x,y)); //G 
+    colors.push(1-getColor(x,y)); //B
+
+	// Redibujar con cada click
+	render( gl, x, y );
+}
+
+function render( gl )
+{
+	// Borra el canvas con el color de fondo
+	gl.clear( gl.COLOR_BUFFER_BIT );
+
+	// Fija el color de TODOS los puntos
+    //const pointColor = 1 -getColor(x, y)
+	//gl.uniform3f(colorFragmento, pointColor, pointColor, pointColor);
+    // Localiza el att del shader posicion
+
+    // Crea buffer, etc ...
+    const vertexBuffer = gl.createBuffer();
+    var coords = gl.getAttribLocation(gl.program, "posicion");
+    gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
+	gl.bufferData( gl.ARRAY_BUFFER, new Float32Array(clicks), gl.STATIC_DRAW );
+    
+    //desasociar buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    
+    //create buffer to store colours
+    const color_buffer = gl.createBuffer ();
+    var color = gl.getAttribLocation(gl.program, "color");
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+    
+    gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
+    gl.vertexAttribPointer( coords, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( coords );
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
+    gl.vertexAttribPointer( color, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( color );
+
+	// Rellena el BO activo con las coordenadas y lo manda a proceso
+	gl.drawArrays( gl.POINTS, 0, clicks.length/3 )	
+    gl.drawArrays( gl.LINE_STRIP, 0, clicks.length/3 )
+}
+
+function getColor(x, y){
+    return Math.max(Math.abs(x), Math.abs(y));
 }
