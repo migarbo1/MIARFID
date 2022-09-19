86202134706c307e73c35a73ac9331f66e66041c
gmrtnz <genis.martinez@biotechvana.com>
2022-09-19 10:00:05 +0200

    Fix some small errors and typos

--- GPC/P1/webgl/js/drawPoints.js
+++ GPC/P1/webgl/js/drawPoints.js
@@ -7,12 +7,12 @@ const V_SHADER_SOURCE = `
     attribute vec3 pointPosition;
     void main(){
         gl_Position = vec4(pointPosition,1.0);
-        gl_pointSize = 10.0;
+        gl_PointSize = 10.0;
     }
 `
 //fragment shader
 const F_SHADER_SOURCE = `
-    unform highp vec3 color;
+    uniform highp vec3 color;
     void main(){
         gl_FragColor = vec4(color, 1.0); //RGBA
     }
@@ -34,7 +34,7 @@ function main(){
     gl.clearColor(0.0,0.0,0.3,1.0)
 
     //locate position parameter in shader
-    const coord = gl.getAttribLocation(gl.program, pointPosition);
+    const coord = gl.getAttribLocation(gl.program, 'pointPosition');
 
     //create buffer and stuff
     const bufferVertex = gl.createBuffer();
