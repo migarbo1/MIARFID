ac6f8cf6cddb6cab29c3b6172e6648479c95a783
Miguel <miguel.garciabohigues@hotmail.com>
2022-09-29 17:55:52 +0200

    tia genetic algorithm

--- TIA/genetics/genetics.py
+++ TIA/genetics/genetics.py
@@ -1,4 +1,3 @@
-from glob import glob
 import json
 import math
 import random
@@ -14,11 +13,12 @@ role_modifier = {#(task_p, worker_p) : modifier
     ("S","J"):1.3,
     ("S","S"):1,
     ("S","C"):0.85,
-    ("C","J"):10000000,
+    ("C","J"):1000,
     ("C","S"):1.3,
     ("C","C"):1,
 }
-seed = random.seed(6)
+seed = random.seed(0)
+sum_fitness = 0
 
 def gen_starting_population(pop_size):
     print("generating starting population...")
@@ -33,6 +33,7 @@ def gen_starting_population(pop_size):
     return population
 
 def compute_fitness(alpha, genotype):
+    global sum_fitness
     fitness = 0
     performing_time = 0
     cost = 0
@@ -44,16 +45,36 @@ def compute_fitness(alpha, genotype):
         cost += task_real_time * prices_hour[gen]
 
     fitness = (alpha * performing_time + (1-alpha) * cost)
+    fitness = 1/fitness #1/fitness because we want to minimize fitness
+    sum_fitness += fitness
     return fitness
         
-def select_specimens():
-    pass
+def select_specimens(population, weights):
+    return random.choices(population, weights, k=2)
 
-def crossover():
-    pass
+def crossover(parent_one, parent_two, n, muration_prob):
 
-def mutation():
-    pass
+    parent_one_split = np.array_split(parent_one, n)
+    parent_two_split = np.array_split(parent_two, n)
+    son_one = []
+    son_two = []
+    for i in range(len(parent_one_split)):
+        son_one += list(parent_one_split[i] if i%2==0 else parent_two_split[i])
+        son_two += list(parent_two_split[i] if i%2==0 else parent_one_split[i])
+    
+    if(random.random() < muration_prob):
+        son_one = mutation(son_one)
+    
+    if(random.random() < muration_prob):
+        son_two = mutation(son_two)
+
+    return son_one, son_two
+
+def mutation(son):
+    source = random.randint(0, len(son)-1)
+    dest = random.randint(0, len(son)-1)
+    son[source],son[dest] = son[dest],son[source]
+    return son
 
 def replacement():
     pass
@@ -64,10 +85,43 @@ def load_problem(path):
     f.close()
     return data
 
+def get_weights_array(fitness_array):
+    weights = []
+    for fit_value in fitness_array:
+        weights.append(fit_value/sum_fitness)
+    return weights
+
 if __name__ == '__main__':
+    population_size = 5
+    alpha = 0.65
+    ancestors_size = 5
+    partitions = 3
+    mutation_prob = 0.05
+    iterations = 1000
+
     _data = load_problem("./simplifiedProblem.json")
-    population = gen_starting_population(5)
-    for gen in population:
-        print(gen)
-        print(compute_fitness(0.5,gen))
-    
+    population = gen_starting_population(population_size)
+    best_sol = (0.0,[])
+    for i in range(iterations):
+        fitness_array = []
+        sum_fitness = 0
+
+        for gen in population:
+            fit = compute_fitness(alpha,gen)
+            if(fit > best_sol[0]):
+                best_sol = (fit, gen)
+            fitness_array.append(compute_fitness(alpha,gen))
+
+        weights = get_weights_array(fitness_array)
+        new_population = []
+        for i in range(ancestors_size):
+            parent_one, parent_two = select_specimens(population, weights)
+            son_one, son_two = crossover(parent_one, parent_two, partitions, mutation_prob)
+            new_population.append(son_one)
+            new_population.append(son_two)
+        population = new_population
+        print(best_sol)
+
+    print("best solution:")
+    print(1/best_sol[0])
+    print(best_sol[1])
